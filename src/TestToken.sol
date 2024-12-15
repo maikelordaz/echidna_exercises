cd5f1181d2cc41eeaca9328f8a544acbf0cc62f2
Maikel Ordaz <maikelordaz@gmail.com>
2024-12-15 12:25:43 -0400

    Echidna exercise 1

---
+++ src/TestToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+
+import "src/Token.sol";
+
+contract TestToken is Token {
+    address echidna = tx.origin;
+
+    constructor() {
+        balances[echidna] = 10_000;
+    }
+
+    function echidna_test_balance() public view returns (bool) {
+        // TODO: add the property
+    }
+}
